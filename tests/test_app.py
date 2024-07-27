9d7d3597c18a0150a27fc5d744cb120745d84a75
Felipe Oliveira <flipeaz342@gmail.com>
2024-07-26 23:27:12 -0400

    add schemas file and format with ruff

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ from fast_zero.app import app
 def test_read_root_deve_retornar_ok_e_hello_world():
     client = TestClient(app)  # Arrange  - Organização
 
-    response = client.get('/')  # Act - Ação
+    response = client.get("/")  # Act - Ação
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'hello world'}
+    assert response.json() == {"message": "hello world"}
