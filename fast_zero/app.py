833912d5b2de828dd4e52b91071b31474fc02c05
Felipe Oliveira <flipeaz342@gmail.com>
2024-07-27 21:36:42 -0400

    add a some schemas and add conftest

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,32 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+
+@app.get("/", response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.post("/users/", response_model=UserPublic, status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get("/users/", response_model=UserList)
+def read_users():
+    return {"user": database}
+
+
+@app.put("/users/{id}/", response_model=UserPublic)
+def update_users(pk: int, user: UserSchema):
+    user_with_id = UserDB(id=pk, **user.model_dump())
+    database[pk - 1] = user_with_id
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,35 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_hello_world():
-    client = TestClient(app)  # Arrange  - Organização
-
+def test_read_root_deve_retornar_ok_e_hello_world(client):
     response = client.get("/")  # Act - Ação
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "hello world"}
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testusername",
+            "email": "test@gmail.com",
+            "password": "test",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        "id": 1,
+        "username": "testusername",
+        "email": "test@gmail.com",
+    }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "user": [{"id": 1, "username": "testusername", "email": "test@gmail.com"}]
+    }
